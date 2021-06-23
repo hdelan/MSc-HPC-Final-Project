da6c6fa7bb4ca536e281a56a8beb43ff6b853c14
Hugh Delaney <hugh.delaney@hotmail.com>
2021-06-23 22:23:24 +0100

    Lanczos

--- matlab-implementation/lanczos.m
+++ matlab-implementation/lanczos.m
@@ -1,4 +1,5 @@
-n = 6; E = 15;
+clear; clc;
+n = 6; E = 10;
 
 v0 = ones(n,1)./sqrt(n);
 
@@ -12,8 +13,27 @@ adj = min( adj + adj.', 1);
 for i = 1:n
     adj(i,i) = 0;
 end
+full(adj)
+ adj = rand(n, n);
+ adj = adj + adj';
 
+q = zeros(n, n);
 
+q(:,1) = rand(n,1);
+q(:,1) = q(:,1)/norm(q(:,1));
+h = zeros(n,n);
 
-plot(graph(adj));
+for j = 1:n
+    w = adj*q(:,1);
+    for i = 1:j
+        h(i,j) = w'*q(:,i);
+        w = w - h(i,j)*q(:,i);
+    end
+    h(j+1,j) = norm(w);
+    q(:,j+1) = w/h(j+1,j);
+end
+
+q*h*(q(:, 1:n))'
+
+%plot(graph(adj));
 
