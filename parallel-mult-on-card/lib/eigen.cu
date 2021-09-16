e6c4390bbd17fcaf91bd39fb4f9d62bd2cc5e600
Hugh Delaney <46290137+hdelan@users.noreply.github.com>
2021-09-16 18:34:26 +0100

    Update eigen.cu

--- parallel-mult-on-card/lib/eigen.cu
+++ parallel-mult-on-card/lib/eigen.cu
@@ -1,7 +1,16 @@
+/**
+ * \file:        eigen.cu
+ * \brief:       Computes the eigendecomposition of the tridiagonal defined by alpha, beta
+ * \author:      Hugh Delaney
+ * \version:     
+ * \date:        2021-09-16
+ */
+
 #include "eigen.h"
 #include "cu_lanczos.h"
 #include <iomanip>
 
+// Overloaded so can be called with floats
 void LAPACKE_dstevd(int layout, char job, int n, float* a, float * b, float * c, int k) {
     LAPACKE_sstevd(layout, job, n, a, b, c, k);
 }
