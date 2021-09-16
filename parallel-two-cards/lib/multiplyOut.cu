57078e0d9138ef618cc73297bb66744821fd5420
Hugh Delaney <46290137+hdelan@users.noreply.github.com>
2021-09-16 18:45:54 +0100

    Update multiplyOut.cu

--- parallel-two-cards/lib/multiplyOut.cu
+++ parallel-two-cards/lib/multiplyOut.cu
@@ -1,5 +1,11 @@
+/**
+ * \file:        multiplyOut.cu
+ * \brief:       Multiply out in serial with multithreaded cblas.
+ * \author:      Hugh Delaney
+ * \version:     
+ * \date:        2021-09-16
+ */
 #include "multiplyOut.h"
-//#include "lapacke.h"
 #include "helpers.h"
 #include <cblas.h>
 #include <iomanip>
@@ -11,17 +17,15 @@ void multOut(lanczosDecomp<T> & L, eigenDecomp<T> & E, adjMatrix & A) {
         
         // Applying function
         for (auto j=0u;j<L.krylov_dim;j++) my_exp_func(E.eigenvalues[j]);
-        //printf("\nSome values from Q serial: %E %E %E\n", L.Q[0], L.Q[L.krylov_dim], L.Q[2*L.krylov_dim]);
+       
         // Elementwise multiplying of f(lambda) by first row of eigenvectors
         for (auto j=0u;j<L.krylov_dim;j++) E.eigenvalues[j] *= L.x_norm * E.eigenvectors[j];
 
-        //print_matrix(3, 1, &E.eigenvalues[0]);
         
         auto k = L.get_krylov();
-        //cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans, n, k, k, 1, L.Q, k, E.eigenvectors, k, 0, QV, k);
         std::vector<T> tmp(k);
-        openblas_set_num_threads(8);
-        //printf("\nSome values from QV serial: %E %E %E\n", QV[0], QV[L.krylov_dim], QV[2*L.krylov_dim]);
+        openblas_set_num_threads(4);
+        // V*f(lambda)
         cblas_dgemv(CblasRowMajor, CblasNoTrans, k, k, 1, E.eigenvectors, k, &E.eigenvalues[0], 1, 0, &tmp[0],1);
         
         // Getting QV*f(lambda)
